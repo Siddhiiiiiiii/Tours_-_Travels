2adf5abfa16f74da498b9bc972e74b5cac85820c
Siddhiiiiiiii <siddhibhosale.patil@gmail.com>
2023-12-25 00:09:22 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Navbar from './Pages/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <Router>
     <div className="App">
-  
+        <Navbar />
+
         <Routes> 
         
         <Route path="/" element={<Home />} />
-        
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+
         </Routes>
      
     </div>
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,25 @@
+// Navbar.js
+
+import React from 'react';
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        {/* Replace 'your_logo.png' with the path to your logo image */}
+        <img src="https://th.bing.com/th/id/OIP.QIg2ejWXEHoJ1xlhx5mQLgHaFe?rs=1&pid=ImgDetMain" />
+        <span> SIDHVED TOURS AND TRAVELS </span>
+      </div>
+      <div className="nav-links">
+        <a href="/">Home</a>
+        <a href="/About">About</a>
+        <a href="/Contact">Contact</a>
+        <a href="/Login">Login</a>
+        <a href="/Register" style={{ color: 'red', background: 'white' }}>Register</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
